049689699e356ba7f3d14c4a592c64a4d3b04623
Cassia <162936759+cassianebel@users.noreply.github.com>
2024-08-01 13:34:07 -0500

    dark mode

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiKey = "0bEwlYlGNxrZUyiX0F0GA0YQsoOeY0l2"
+const apiKey = "0bEwlYlGNxrZUyiX0F0GA0YQsoOeY0l2";
 const url = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`;
 let windowWidth = window.innerWidth;
 
@@ -9,39 +9,39 @@ async function fetchTopStories() {
     console.log(data.results);
     displayStories(data.results);
   } catch (error) {
-    console.error('Error fetching the top stories:', error);
+    console.error("Error fetching the top stories:", error);
   }
 }
 
 function displayStories(stories) {
-  const storiesContainer = document.getElementById('stories');
-  storiesContainer.innerHTML = '';
+  const storiesContainer = document.getElementById("stories");
+  storiesContainer.innerHTML = "";
 
   stories.forEach((story, index) => {
-    const storyLink = document.createElement('a');
+    const storyLink = document.createElement("a");
     storyLink.href = story.url;
-    storyLink.target = '_blank';
-    storyLink.className = 'story';
+    storyLink.target = "_blank";
+    storyLink.className = "story";
 
-    const storyImg = document.createElement('img');
+    const storyImg = document.createElement("img");
     // if (windowWidth >= 768 && index === 0) {
     //   storyImg.src = story.multimedia[0].url;
     //   storyImg.alt = story.multimedia[0].caption;
     // } else {
-      storyImg.src = story.multimedia[1].url;
-      storyImg.alt = story.multimedia[1].caption;
+    storyImg.src = story.multimedia[1].url;
+    storyImg.alt = story.multimedia[1].caption;
     // }
     storyLink.appendChild(storyImg);
-    
-    const storyTitle = document.createElement('h2');
+
+    const storyTitle = document.createElement("h2");
     storyTitle.textContent = story.title;
-    
-    const storyAbstract = document.createElement('p');
+
+    const storyAbstract = document.createElement("p");
     storyAbstract.textContent = story.abstract;
 
-    const storyText = document.createElement('div');
-    storyText.className = 'text';
-    
+    const storyText = document.createElement("div");
+    storyText.className = "text";
+
     storyText.appendChild(storyTitle);
     storyText.appendChild(storyAbstract);
     storyLink.appendChild(storyText);
@@ -49,10 +49,20 @@ function displayStories(stories) {
   });
 }
 
-window.addEventListener('resize', () => {
-    windowWidth = window.innerWidth;
+window.addEventListener("resize", () => {
+  windowWidth = window.innerWidth;
 });
 
 // Fetch top stories on page load
 fetchTopStories();
 
+// Dark mode toggle
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleCheckbox = document.getElementById("dark-mode-toggle");
+  const htmlElement = document.documentElement;
+
+  toggleCheckbox.addEventListener("change", () => {
+    const newTheme = toggleCheckbox.checked ? "dark" : "light";
+    htmlElement.setAttribute("data-theme", newTheme);
+  });
+});
